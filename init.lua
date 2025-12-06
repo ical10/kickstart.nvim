b49b6e7a907a53572a48172bec4ddd53c6c07fbd
ical10 <mochhusnir@gmail.com>
2025-12-06 17:37:14 +0800

    feat: add matchit macros for extending % key

--- init.lua
+++ init.lua
@@ -222,6 +222,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Enable matchit plugin for extended % key functionality
+--  See `:help matchit`
+vim.cmd.source(vim.env.VIMRUNTIME .. '/macros/matchit.vim')
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
